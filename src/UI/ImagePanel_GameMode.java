61f9540c61e63502838663154256e8de559dd193
TeyYongZhun <teyyongzhun@gmail.com>
2025-11-26 19:18:42 +0800

    update UI Game Mode

--- src/UI/ImagePanel_GameMode.java
+++ src/UI/ImagePanel_GameMode.java
@@ -9,6 +9,8 @@ public class ImagePanel_GameMode extends JPanel {
 
     private Image backgroundImage;
     private HomeButton homeButton; //declare home button
+    private ChallengeModeButton challengeButton; //declare challenge mode button
+    private StoryModeButton storyButton; // declare story mode button
     private MainFrame mainFrameRef;
     private String username;
 
@@ -24,15 +26,22 @@ public class ImagePanel_GameMode extends JPanel {
 
         //1.Create Home Button
         homeButton = new HomeButton("images/home_button.png",this.mainFrameRef, this.username);
-
         homeButton.addActionListener(e -> {
             if (mainFrameRef != null) {
                 // Switch back to Home, passing the username back!
                 mainFrameRef.changeToHome(this.username); 
             }
         });
-
         add(homeButton);
+
+        //2.Create Challenge Mode Button
+        challengeButton = new ChallengeModeButton("images/challengemode_button.png");
+        add(challengeButton);
+
+        //3.Create Story Mode Button
+        storyButton = new StoryModeButton("images/storymode_button.png");
+        add(storyButton);
+
 
 
         addComponentListener(new ComponentAdapter() {
@@ -54,7 +63,27 @@ public class ImagePanel_GameMode extends JPanel {
         homeButton.setBounds(30, 30, homeSize, homeSize);
         homeButton.resizeIcon(homeSize);
 
-        //
+        // 2. Set Challenge Mode Button
+        int cHeight = (int)(h * 0.19); 
+        int cWidth  = (int)(cHeight * 2.5); 
+
+        int challengeX = (int) (w * 0.57);
+        int challengeY = ((h / 2) - (cHeight / 2)) + 250;
+
+
+        challengeButton.setBounds(challengeX, challengeY, cWidth, cHeight);
+        challengeButton.resizeIcon(cWidth, cHeight);
+
+        // 3. Set Story Mode Button
+        int storyHeight = (int)(h * 0.19); 
+        int storyWidth  = (int)(cHeight * 2.5); 
+
+        int storyX = (int) (w * 0.10);
+        int storyY = ((h / 2) - (storyHeight / 2)) + 250;
+
+        storyButton.setBounds(storyX, storyY, storyWidth, storyHeight);
+        storyButton.resizeIcon(storyWidth, storyHeight);
+
     }
 
     @Override
---
+++ src/UI/StoryModeButton.java
@@ -0,0 +1,37 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StoryModeButton extends JButton {
+
+    private Image originalImage;
+
+    public StoryModeButton(String imagePath) {
+        // 1. Load the image
+        this.originalImage = new ImageIcon(imagePath).getImage();
+
+        // 2. Make the button transparent (Ghost style)
+        setBorderPainted(false);
+        setContentAreaFilled(false);
+        setFocusPainted(false);
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        // 3. Add a simple click action
+        addActionListener(e -> {
+            System.out.println("Story Mode Button Clicked!");
+            JOptionPane.showMessageDialog(this, "Story Mode Feature Coming Soon!");
+        });
+    }
+
+    /**
+     * Resizes the icon image to fit a specific size.
+     * Call this from the main panel when the window resizes.
+     */
+    public void resizeIcon(int width, int height) {
+        if (width > 0 && height > 0) {
+            Image scaled = originalImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            setIcon(new ImageIcon(scaled));
+        }
+    }
+}
